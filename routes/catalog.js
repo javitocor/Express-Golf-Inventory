f058ddc6f86aced80ebd312a50fe9a26de08371a
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-09 20:06:54 +0200

    set up mongoose and catalog routes in app.js

---
+++ routes/catalog.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+// Require controllers.
+var item_controller = require('../controllers/itemController'); 
+var categorie_controller = require('../controllers/categorieController');
+
+// Item routes
+
+// Categorie routes
+
+
