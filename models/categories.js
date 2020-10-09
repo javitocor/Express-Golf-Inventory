1753f13366e15f515ae4f7a1f84ef948b9d5d4d8
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-09 19:59:27 +0200

    implement models

---
+++ models/categories.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CategorieSchema = new Schema({
+    name: {type: String, required: true, min: 3, max: 20},
+    description: {type: String, required: true, min: 5, max: 100}
+});
+
+// Virtual for this categorie instance URL.
+CategorieSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/'+this._id;
+});
+
+// Export model.
+module.exports = mongoose.model('Categorie', CategorieSchema);
---
+++ models/items.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema({
+    name: {type: String, required: true, min: 3, max: 20},
+    description: {type: String, required: true, min: 5, max: 150},
+    categorie: {type: Schema.ObjectId, ref: 'Categorie', required: true},
+    price: {type: Number, required: true, min: 0},
+    stock: {type: Number, required: true, min: 0},
+});
+
+// Virtual for this Item instance URL.
+ItemSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/'+this._id;
+});
+
+// Export model.
+module.exports = mongoose.model('Item', ItemSchema);
