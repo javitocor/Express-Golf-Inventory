2debd3cd35ed4f42510d8aa96237604229ad2c8c
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-12 07:49:19 +0200

    implement categories controller

--- controllers/categorieController.js
+++ controllers/categorieController.js
@@ -95,14 +95,95 @@ exports.category_create_post = [
   }
 ];
 exports.category_update_get = function(req, res) {
-
+  Categorie.findById(req.params.id, function(err, cat) {
+    if (err) { return next(err); }
+    if (cat==null) { // No results.
+        var err = new Error('Categorie not found');
+        err.status = 404;
+        return next(err);
+    }
+    // Success.
+    res.render('category_form', { title: 'Update Category', cat: cat });
+});
 }
-exports.category_update_post = function(req, res) {
+exports.category_update_post = [
+   
+  // Validate and sanitze the name field.
+  body('name', 'Name required').trim().isLength({ min: 1 }).escape(),
+  body('description', 'Description required').trim().isLength({ min: 1 }).escape(),
 
-}
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+      // Extract the validation errors from a request .
+      const errors = validationResult(req);
+
+  // Create a genre object with escaped and trimmed data (and the old id!)
+      var category = new Genre(
+        {
+        name: req.body.name,
+        description: req.body.description,
+        _id: req.params.id
+        }
+      );
+
+
+      if (!errors.isEmpty()) {
+          // There are errors. Render the form again with sanitized values and error messages.
+          res.render('category_form', { title: 'Update Category', category: category, errors: errors.array()});
+      return;
+      }
+      else {
+          // Data from form is valid. Update the record.
+          Categorie.findByIdAndUpdate(req.params.id, category, {}, function (err,thecat) {
+              if (err) { return next(err); }
+                 // Successful - redirect to genre detail page.
+                 res.redirect(thecat.url);
+              });
+      }
+  }
+];
 exports.category_delete_get = function(req, res) {
-
+  async.parallel({
+    category: function(callback) {
+        Categorie.findById(req.params.id).exec(callback);
+    },
+    cat_items: function(callback) {
+        Item.find({ 'categorie': req.params.id }).exec(callback);
+    },
+}, function(err, results) {
+    if (err) { return next(err); }
+    if (results.category==null) { // No results.
+        res.redirect('/catalog/categories');
+    }
+    // Successful, so render.
+    res.render('category_delete', { title: 'Delete Category', category: results.category, cat_items: results.cat_items } );
+});
 }
 exports.category_delete_post = function(req, res) {
-  
+  async.parallel({
+    category: function(callback) {
+        Categorie.findById(req.params.id).exec(callback);
+    },
+    cat_items: function(callback) {
+        Item.find({ 'categorie': req.params.id }).exec(callback);
+    },
+}, function(err, results) {
+    if (err) { return next(err); }
+    // Success
+    if (results.cat_items.length > 0) {
+        // Cat has itms. Render in same way as for GET route.
+        res.render('category', { title: 'Delete Category', category: results.category, cat_items: results.cat_items } );
+        return;
+    }
+    else {
+        // Genre has no books. Delete object and redirect to the list of categories.
+        Categorie.findByIdAndRemove(req.body.id, function deleteGenre(err) {
+            if (err) { return next(err); }
+            // Success - go to categories list.
+            res.redirect('/catalog/categories');
+        });
+
+    }
+});
 }
