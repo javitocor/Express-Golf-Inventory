d396b7d9da63815c64e8be0933d3cc2489972fea
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-12 07:35:23 +0200

    refactor items controller

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -112,7 +112,7 @@ body('categorie.*').escape(),
 
             // Mark our selected categories as checked.
             for (let i = 0; i < results.categories.length; i++) {
-                if (book.categories.indexOf(results.categories[i]._id) > -1) {
+                if (item.categories.indexOf(results.categories[i]._id) > -1) {
                     results.categories[i].checked='true';
                 }
             }
@@ -160,7 +160,7 @@ exports.item_update_get = function(req, res) {
 }
 exports.item_update_post  = [
 
-  // Convert the genre to an array.
+  // Convert the categorie to an array.
   (req, res, next) => {
       if(!(req.body.categorie instanceof Array)){
           if(typeof req.body.categorie==='undefined')
@@ -197,7 +197,7 @@ exports.item_update_post  = [
       if (!errors.isEmpty()) {
           // There are errors. Render form again with sanitized values/error messages.
 
-          // Get all authors and genres for form
+          // Get all items and categories for form
           async.parallel({
               items: function(callback) {
                   Item.find(callback);
@@ -220,10 +220,10 @@ exports.item_update_post  = [
       }
       else {
           // Data from form is valid. Update the record.
-          Book.findByIdAndUpdate(req.params.id, book, {}, function (err,thebook) {
+          Item.findByIdAndUpdate(req.params.id, item, {}, function (err,theitem) {
               if (err) { return next(err); }
-                 // Successful - redirect to book detail page.
-                 res.redirect(thebook.url);
+                 // Successful - redirect to item detail page.
+                 res.redirect(theitem.url);
               });
       }
   }
@@ -233,35 +233,20 @@ exports.item_delete_get = function(req, res) {
     item: function(callback) {
         Item.findById(req.body.id).populate('categorie').exec(callback);
     },
-    categories: function(callback) {
-        Categorie.find({ 'item': req.body.id }).exec(callback);
-    },
 }, function(err, results) {
     if (err) { return next(err); }
     // Success
-    if (results.categories.length > 0) {
-        // Book has book_instances. Render in same way as for GET route.
-        res.render('book_delete', { title: 'Delete Item', item: results.item, categories: results.categories } );
-        return;
-    }
-    else {
-        // Book has no BookInstance objects. Delete object and redirect to the list of books.
-        Item.findByIdAndRemove(req.body.id, function deleteBook(err) {
-            if (err) { return next(err); }
-            // Success - got to books list.
-            res.redirect('/catalog/items');
-        });
-
-    }
+    Item.findByIdAndRemove(req.body.id, function deleteBook(err) {
+      if (err) { return next(err); }
+      // Success - got to items list.
+      res.redirect('/catalog/items');
+  });
 });
 }
 exports.item_delete_post = function(req, res) {
   async.parallel({
     item: function(callback) {
         Item.findById(req.params.id).populate('categorie').exec(callback);
-    },
-    categories: function(callback) {
-        Categorie.find(callback);
     },
     }, function(err, results) {
         if (err) { return next(err); }
@@ -271,15 +256,11 @@ exports.item_delete_post = function(req, res) {
             return next(err);
         }
         // Success.
-        // Mark our selected genres as checked.
-        for (var all_g_iter = 0; all_g_iter < results.categories.length; all_g_iter++) {
-            for (var item_g_iter = 0; book_g_iter < results.item.genre.length; item_g_iter++) {
-                if (results.categories[all_g_iter]._id.toString()==results.item.genre[item_g_iter]._id.toString()) {
-                    results.categories[all_g_iter].checked='true';
-                }
-            }
-        }
-        res.render('item_form', { title: 'Update item', categories:results.categories, item: results.item });
+        Item.findByIdAndRemove(req.body.id, function deleteBook(err) {
+          if (err) { return next(err); }
+          // Success - got to items list.
+          res.redirect('/catalog/items');
+      });
     });
 }
 
